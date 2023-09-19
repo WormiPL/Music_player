8ca61333aa1a95a4e303bf8234aa7db7f83f5ae5
wormipl <konrad.belzowski453@gmail.com>
2023-09-19 09:58:18 +0200

    Theme change

--- script.js
+++ script.js
@@ -2,30 +2,32 @@
 const toggle_theme = document.querySelector('#dark_mode')
 const background_image = document.querySelector('#bg_light')
 const navtop = document.querySelector('.navtop')
-const text = document.querySelector('.navtop a')
-const text2 = document.querySelector('.dropdown a')
-const dropdown = document.querySelector('.dropdown-content')
-const dropdown_text = document.querySelector('.dropdown-content a')
+const text = document.querySelectorAll('.navtop li a')
+const dropdown_content = document.querySelector('.dropdown-content')
+
 
 let state = true //true dla białego
 
+const white_theme = ()=>{
+    background_image.src = './img/bg1.jpg'
+    navtop.style.backgroundColor = '#c7c7c7'
+    dropdown_content.style.backgroundColor = '#c7c7c7'
+    text.forEach((el)=>{
+        el.style.color = '#333'
+    })
+}
+
+const black_theme = ()=>{
+    background_image.src = './img/bg2.jpg'
+    navtop.style.backgroundColor = '#333'
+    dropdown_content.style.backgroundColor = '#333'
+    text.forEach((el)=>{
+        el.style.color = '#c7c7c7'
+    })
+}
+
 toggle_theme.addEventListener('click',()=>{
-    if(state){
-        state = false
-        background_image.src = './img/bg2.jpg'
-        navtop.style.backgroundColor="#333"
-        text.style.color= '#c7c7c7'
-        text2.style.color= '#c7c7c7'
-        dropdown.style.backgroundColor='#333'
-        dropdown_text.style.color= '#c7c7c7'
-    }else{
-        state = true
-        background_image.src = './img/bg1.jpg'
-        navtop.style.backgroundColor="#c7c7c7";
-        text.style.color= '#333'
-        text2.style.color= '#333'
-        dropdown.style.backgroundColor='#c7c7c7'
-        dropdown_text.style.color= '#333'
-    }
+    state =!state
+    state ? white_theme() : black_theme()
 })
 
